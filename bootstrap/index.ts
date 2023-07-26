63d1c6f28dce1f61eab68d00dac7f871fe2bc807
Audacious Tux <tangimhossain1@gmail.com>
2023-07-26 10:55:44 +0000

    upgrade to esnext

--- bootstrap/index.ts
+++ bootstrap/index.ts
@@ -1,6 +1,10 @@
 import * as k8s from "@pulumi/kubernetes";
-import * as kx from "@pulumi/kubernetesx";
 import { getStack, StackReference, getOrganization } from "@pulumi/pulumi";
 
-export const other = new StackReference(`${getOrganization()}/manifests/${getStack()}`);
+export const manifest_stack = new StackReference(`${getOrganization()}/manifests/${getStack()}`);
 
+const argocd = await manifest_stack.getOutputValue("argocd")
+
+const argo_cd = new k8s.yaml.ConfigGroup("argo-cd", {
+    files: [`../manifests/${argocd}/0-crd/*.yaml`, `../manifests/${argocd}/1-manifest/*.yaml`]
+});
