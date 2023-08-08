e7d28f17ccc189d7a96b6fc0d5949a29edb66aa1
Audacious Tux <tangimhossain1@gmail.com>
2023-08-08 13:19:30 +0000

    ..

--- bootstrap/main.ts
+++ bootstrap/main.ts
@@ -1,2 +1,11 @@
 import * as k8s from "@pulumi/kubernetes";
-import { getStack, StackReference, getOrganization } from "@pulumi/pulumi";
+import { getStack, StackReference, getOrganization, Config } from "@pulumi/pulumi";
+
+const k8sProvider = new k8s.Provider("k8s");
+
+let config = new Config();
+let manifest = config.require("manifest");
+
+new k8s.yaml.ConfigFile("argo-cd", {
+    file: manifest,
+}, { provider: k8sProvider });
--- manifests/src/main.ts
+++ manifests/src/main.ts
@@ -1,18 +1,20 @@
 import { Construct } from 'constructs';
 import { App, Chart, ChartProps, Helm } from 'cdk8s';
 import { KubeDeployment, KubeService, IntOrString, KubeNamespace } from '../imports/k8s.js';
+import { Application } from "../imports/argoproj.io.js"
 
 export class ArgoCD extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = {}) {
     super(scope, id, props);
 
-    const namespace = new KubeNamespace(this, 'argocd-ns', {
+    const namespace = new KubeNamespace(this, 'ns', {
       metadata: {
         name: 'argocd',
       },
     });
 
-    const helm = new Helm(this, 'argocd', {
+    const helm = new Helm(this, 'helm', {
+      releaseName: "argocd",
       chart: "argo/argo-cd",
       helmFlags: [
         "--set", "installCRDs=true",
@@ -20,26 +22,54 @@ export class ArgoCD extends Chart {
       ],
       values: {
         "redis-ha": {
-          enabled: true
+          // enabled: true
         },
         controller: {
           replicas: 1
         },
         server: {
-          autoscaling: {
-            enabled: true,
-            minReplicas: 2
-          }
+          // autoscaling: {
+          //   enabled: true,
+          //   minReplicas: 2
+          // }
         },
         repoServer: {
-          autoscaling: {
-            enabled: true,
-            minReplicas: 2
-          },
+          // autoscaling: {
+          //   enabled: true,
+          //   minReplicas: 2
+          // },
         },
         applicationSet: {
-          replicaCount: 2
+          // replicaCount: 2
         }
+      },
+    });
+
+    const app = new Application(this, 'app', {
+      metadata: {
+        name: helm.releaseName,
+        namespace: namespace.name,
+      },
+      spec: {
+        destination: {
+          namespace: namespace.name,
+          server: 'https://kubernetes.default.svc',
+        },
+        project: 'default',
+        source: {
+          path: 'manifests/dist',
+          directory: {
+            include: "argocd.k8s.yaml"
+          },
+          repoUrl: "https://github.com/audacioustux/sdp.git",
+          targetRevision: 'HEAD',
+        },
+        syncPolicy: {
+          automated: {
+            prune: true,
+            selfHeal: true,
+          },
+        },
       },
     });
   }
