17d30106a761a2fc1d478932be6fe8dc6734505b
Audacious Tux <tangimhossain1@gmail.com>
2023-12-24 14:50:02 +0000

    remove minikube on startup, add pulumi feature

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,27 +1,3 @@
 #!/usr/bin/env bash
 
 set -eax
-
-# set the default profile
-minikube profile zerosdp
-# minikube feature mounts ~/.minikube to devcontainer
-# so need to delete previous profile if it exists
-minikube delete
-# start minikube
-minikube start --cpus 6 --memory 8g --driver=docker --cni=false
-# install ciliium
-cilium install
-# enable hubble
-cilium hubble enable
-# enable volume snapshots
-minikube addons enable volumesnapshots
-# use csi hostpath driver
-minikube addons enable csi-hostpath-driver
-# set the default storage class
-minikube addons disable storage-provisioner
-minikube addons disable default-storageclass
-kubectl patch storageclass csi-hostpath-sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-# use minikube's docker daemon
-LINE='eval $(minikube docker-env)'
-FILE=~/.zshrc
-grep -xqF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
 set -eax
-
-minikube status || minikube start 
-
-tunnel-minikube() {
-  minikube tunnel --bind-address "0.0.0.0"
-}
-
-parallel --halt now,fail=1 \
-    --linebuffer \
-    -j0 ::: \
-        "tunnel-minikube" 
